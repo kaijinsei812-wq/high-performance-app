a51a5380c24dbb29231e40ce9f60a70efc424a1a
kaijinsei812-wq <kaijinsei812@gmail.com>
2026-03-19 06:05:16 +0900

    🔒 security: CORS設定をlocalhost限定に変更

--- app/main.py
+++ app/main.py
@@ -39,7 +39,14 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost",
+        "http://localhost:3000",
+        "http://localhost:8000",
+        "http://127.0.0.1",
+        "http://127.0.0.1:3000",
+        "http://127.0.0.1:8000",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
